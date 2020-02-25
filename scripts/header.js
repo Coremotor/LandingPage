32700e39d00fa258f7a0bb5ff172bd5d66b1ea88
Coremotor <Coremotor@users.noreply.github.com>
2020-02-25 12:52:22 +0300

    Общие правки

--- scripts/header.js
+++ scripts/header.js
@@ -1,21 +1,21 @@
-let headerElem = document.querySelector('.js-header');
-let headerBtnElem = document.querySelector('.js-header__btn');
+// let headerElem = document.querySelector('.js-header');
+// let headerBtnElem = document.querySelector('.js-header__btn');
 
 
-//ф-ия смены текста в элементе DOM при определенном размере окна
-function setupForWidth(mql) {
-  if (mql.matches) {
+// //ф-ия смены текста в элементе DOM при определенном размере окна
+// function setupForWidth(mql) {
+//   if (mql.matches) {
     
-    headerBtnElem.innerHTML = '';
-  } else {
-    headerBtnElem.innerHTML = 'Заказать звонок';
-  }
-}
+//     headerBtnElem.innerHTML = '';
+//   } else {
+//     headerBtnElem.innerHTML = 'Заказать звонок';
+//   }
+// }
 
-//получение результата "медиа запроса"
-let mql = window.matchMedia("(max-width: 1024px)");
+// //получение результата "медиа запроса"
+// let mql = window.matchMedia("(max-width: 1024px)");
 
-// Добавим прослушку на смену результата
-mql.addListener(setupForWidth); 
+// // Добавим прослушку на смену результата
+// mql.addListener(setupForWidth); 
  
-setupForWidth(mql);
+// setupForWidth(mql);
