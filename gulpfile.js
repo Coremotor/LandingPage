2003c4caf8b84ea4408fa1e6d4183852b8ef61fb
Coremotor <Coremotor@users.noreply.github.com>
2020-03-12 15:33:47 +0300

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,89 @@
-var gulp      = require('gulp'), // Подключаем Gulp
-		sass        = require('gulp-sass'), //Подключаем Sass пакет,
-		browserSync = require('browser-sync').create(), // Подключаем Browser Sync
-		autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
-		cssnano = require('gulp-cssnano');
+"use strict";
 
-gulp.task('sass', function(){ // Создаем таск Sass
-	return gulp.src('./styles/styles.scss') // Берем источник
+// Задаем стандартные пути
+const paths = {
+	styles: {
+		src: 'styles/styles.scss',
+		dest: 'public/styles'
+	},
+	scripts: {
+		src: '',
+		dest: ''
+	},
+	img: {
+		src: 'img/*.{png,jpg}',
+		dest: 'public/img'
+	}
+}
+
+const gulp = require('gulp'); // Подключаем Gulp
+const	sass = require('gulp-sass'); //Подключаем Sass пакет,
+const	browserSync = require('browser-sync').create(); // Подключаем Browser Sync
+const	autoprefixer = require('gulp-autoprefixer'); // Подключаем библиотеку для автоматического добавления префиксов
+const	cssnano = require('gulp-cssnano'); // Подключаем библиотеке для минификации CSS файлов
+const rename = require('gulp-rename'); // Подключаем бибилиотеку для переименования файлов
+const htmlmin = require('gulp-htmlmin'); // Подключаем библиотеку для минификации HTML файлов
+const tinymin = require('gulp-tinypng'); // Подключаем библиотеку для оптимизации картинок через  ресурс tinypng
+const imageMin = require("gulp-imagemin"); // Подключаем библиотеку для оптимизации картинок gulp-плагином
+const webp = require("gulp-webp"); // Подключаем библиотеку дляконвертации картинок в формат webP
+
+
+function sassConvertInCss(){ // Создаем таск Sass
+	return gulp.src(paths.styles.src) // Берем источник
 		.pipe(sass().on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass и логируем ошибки
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-		.pipe(gulp.dest('./styles')) // Выгружаем результата в папку app/css
-		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
+		.pipe(gulp.dest('./styles')) // Выгружаем результат
+		.pipe(browserSync.reload({stream: true})) // Обновляем страницу при изменении CSS стилей
+};
 
-gulp.task('cssmin', function() {
-	return gulp.src('./styles/styles.css')
+function cssmin() {
+	return gulp.src('styles/styles.css') // Берем источник
 		.pipe(cssnano())
-		.pipe(gulp.dest('./styles/cssmin'));
-});
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest(paths.styles.dest)) // Выгружаем результат
+};
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
+function minifyHtml() {
+  return gulp.src('*.html') // указываем пути к файлам .html
+  .pipe(htmlmin({
+    collapseWhitespace: true, // удаляем все переносы
+    removeComments: true // удаляем все комментарии
+  }))
+	.pipe(gulp.dest('public/')) // оптимизированные файлы .html переносим на продакшен
+	.pipe(browserSync.reload({stream: true})) // Обновляем страницу при изменении HTML разметки
+};
+
+//tinypng api key WfKW8hdYnBwNbXK2vCS7cT0WnXxNrGTt
+
+function tinyPngJpg() {
+	gulp.src(paths.img.src) // Берем источник
+			.pipe(tinymin('WfKW8hdYnBwNbXK2vCS7cT0WnXxNrGTt')) //api key для ресурса tinypng юзать не более 500 раз в месяц
+			.pipe(gulp.dest(paths.img.dest)) // Выгружаем результат
+};
+
+function images() {
+return gulp.src(paths.img.src) // Берем источник
+	.pipe(imageMin([
+		imageMin.optipng({optimizationLevel: typeOfCompression}),
+		imageMin.jpegtran({progressive: true}),
+		imageMin.svgo({
+			plugins: [
+						{removeViewBox: true},
+						{cleanupIDs: false}
+					]
+			})
+	]))
+	.pipe(gulp.dest(paths.img.dest)) // Выгружаем результат
+};
+
+function webpConvert() {
+return gulp.src(paths.img.src) // Берем источник
+	.pipe(webp({quality: 95,
+							lossless: false}))
+	.pipe(gulp.dest(paths.img.dest)) // Выгружаем результат
+};
+
+function browserSyncRun() { // Создаем таск browser-sync
 	browserSync.init({ // Выполняем browserSync
 		server: { // Определяем параметры сервера
 			baseDir: './' // Директория для сервера
@@ -27,9 +92,23 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 		open: true,
 		notify: false // Отключаем уведомления
 	});
-});
+};
 
-gulp.task('watch', function() {
-	gulp.watch('./styles/**/*.scss', gulp.parallel('sass')); // Наблюдение за sass файлами
-});
-gulp.task('default', gulp.parallel('sass', 'cssmin', 'browser-sync', 'watch'));
+function watch() {
+	gulp.watch('styles/**/*.scss', gulp.parallel(sassConvertInCss)) // Наблюдение за sass файлами и конвертация
+	gulp.watch('*.html', gulp.parallel(minifyHtml)) // Наблюдение за HTML файлами и конвертация
+};
+
+exports.sassConvertInCss = sassConvertInCss;
+exports.cssmin = cssmin;
+exports.minifyHtml = minifyHtml;
+exports.browserSyncRun = browserSyncRun;
+exports.tinyPngJpg = tinyPngJpg;
+exports.images = images;
+exports.webpConvert = webpConvert;
+exports.watch = watch;
+exports.default = gulp.parallel(sassConvertInCss, cssmin, browserSyncRun, watch);
+
+
+
+
